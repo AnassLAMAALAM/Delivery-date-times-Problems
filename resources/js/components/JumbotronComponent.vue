a372e119845d800f756aaf201c49ccaea798208b
anasslam21 <anasslam69@gmail.com>
2020-02-24 04:30:15 +0100

    create components

---
+++ resources/js/components/JumbotronComponent.vue
@@ -0,0 +1,21 @@
+<template>
+      <div class="row section">
+    <div class="col-md-6">
+    <div class="heading" >
+    <h1>Just drive. </h1>
+    <h1>we will do the rest</h1>
+    <div>
+      <p> all exclusive,usage based car subscriptions </p>
+      <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Get stared</button>
+    </form>
+      
+    </div>
+    </div>
+  </div>
+    <div class="col-md-6">
+    <img  :src="'/images/cover1.jpg'"  class="img-fluid" alt="Responsive image">
+  </div>
+</div>
+</template>
---
+++ resources/js/components/PricingComponent.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class="pricing-table">
+        <div class="container-fluid">
+            <div class="block-heading">
+              <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-md-5 col-lg-4" >
+                  <div class="bading"></div>
+                    <div class="item">
+                        <div class="heading">
+                            <h3>FlexPass</h3>
+                            <p>Get 2 days / month</p>
+                        </div>
+                        <div class="price">
+                           <h4>$159 </h4> <p>/Month</p> 
+                        </div>
+                        <div class="features">
+                            <h4><span class="value">Lorem Ipsum is simply dummy text of the printing and typesetting industrys</span></h4>
+                        </div>
+                         <button  class="btn btn-block btn-secondary" type="submit">Start with Flex</button>
+                        <a href="#" > View plan details <i class="fas fa-arrow-right"></i></a>
+
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                    <div class="item">
+                        <div class="heading">
+                            <p>RECOMMENDED</p>
+                            <h3>Getaway</h3>
+                            <p>Get 4 days / month</p>
+                        </div>
+                        <div class="price">
+                           <h4>$219 </h4> <p >/Month</p> 
+                        </div>
+                        <div class="features">
+                            <h4><span class="value">Lorem Ipsum is simply dummy text of the printing and typesetting industrys</span></h4>
+                        </div>
+                        <button  class="btn btn-block btn-secondary" type="submit">Start with Getway</button>
+                        <a href="#" > View plan details <i class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-5 col-lg-4">
+                  <div class="bading"></div>
+                    <div class="item">
+                        <div class="heading">
+                            <h3>Freedom</h3>
+                            <p>Get 8 days / month</p>
+                        </div>
+                        <div class="price">
+                           <h4>$983 </h4> <p >/Month</p> 
+                        </div>
+                        <div class="features">
+                            <h4><span class="value">Lorem Ipsum is simply dummy text of the printing and typesetting industrys</span></h4>
+                        </div>
+                        <button  class="btn btn-block btn-secondary" type="submit">Start with Freedom </button>
+                        <a href="#" > View plan details <i class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
